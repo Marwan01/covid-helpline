9bf8bbe24ba81c4ba7ee4e6b96c73e7a1ab34805
ajay <ajay@ajayraj.co>
2020-03-24 02:15:14 -0700

    Added basic rate limit code depending on verifications, waiting on Twilio

--- src/main.py
+++ src/main.py
@@ -22,6 +22,16 @@ app = Flask(__name__)
 def sms_ahoy_reply():
     number = request.form['From']
     message_body = request.form['Body']
+
+    # Sends verification text... seems to work with rate limiting set, must see if implementation allows using this to MessagingResponse() instead. Contacting Twilio support
+    verification = client_twillio.verify \
+                     .services(limit_service.sid) \
+                     .verifications \
+                     .create(rate_limits={
+                          'end_user_phone_number': '##########' # Sparse documentation for this, contacted support to get more info
+                      }, to=number, channel='sms')
+
+
     save_text(bucket,number,message_body)
     msg_out_response = handle_message(bucket,number,message_body)
     resp = MessagingResponse()
@@ -41,8 +51,31 @@ def phone_receiver():
     return str(resp)
 
 client_twillio = Client(account_sid, auth_token)
+
+# Google Cloud client
 client = storage.Client(project='covid-helpline')
 bucket = client.get_bucket('covid-sms')
+
+
+# Verification Service prerequisite for rate limiting
+limit_service = client_twillio.verify.services.create(friendly_name='Rate Limit Service')
+
+rate_limit = client_twillio.verify \
+    .services(limit_service.sid) \
+    .rate_limits \
+    .create(
+         description='Limit verifications by Phone Number',
+         unique_name='end_user_phone_number'
+     )
+
+
+limit_bucket = client_twillio.verify.services(limit_service.sid) \
+                      .rate_limits(rate_limit.sid) \
+                      .buckets \
+                      .create(max=10, interval=14440)   # Cap at 10 requests per 4 hours
+
+
+
 
 if __name__ == "__main__":
     app.run( host='0.0.0.0',port=8080, debug=True)
